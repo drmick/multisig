3b2989655c683484f79316424f4c450f162b8313
Maksim G <infiltrator53@gmail.com>
2022-06-13 11:55:50 +0500

    SPL tests

--- tests/multisig.js
+++ tests/multisig.js
@@ -1,5 +1,11 @@
 const anchor = require("@project-serum/anchor");
 const assert = require("assert");
+const splToken = require('@solana/spl-token');
+const {AuthorityType} = require("@solana/spl-token");
+const {createTransferCheckedInstruction} = require("@solana/spl-token");
+const {getAssociatedTokenAddress, getMint} = require("@solana/spl-token");
+const {ASSOCIATED_TOKEN_PROGRAM_ID} = require("@solana/spl-token");
+const {TOKEN_PROGRAM_ID} = require("@solana/spl-token");
 
 describe("multisig", () => {
   // Configure the client to use the local cluster.
@@ -123,7 +129,7 @@ describe("multisig", () => {
         // Change the signer status on the vendor signer since it's signed by the program, not the client.
         .map((meta) =>
           meta.pubkey.equals(multisigSigner)
-            ? { ...meta, isSigner: false }
+            ? {...meta, isSigner: false}
             : meta
         )
         .concat({
@@ -176,4 +182,164 @@ describe("multisig", () => {
       assert.ok(err.message.includes("0x1778"));
     }
   });
+
+  it("SPL Transaction", async () => {
+    // Step 1: Create multisig
+    let provider = program.provider;
+    const multisig = anchor.web3.Keypair.generate();
+
+    const [multisigSigner, nonce] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [multisig.publicKey.toBuffer()],
+        program.programId
+      );
+
+    const owners = [program.provider.wallet.publicKey];
+    const name = "test";
+    const threshold = new anchor.BN(1);
+    await program.rpc.createMultisig(
+      {
+        owners,
+        threshold,
+        nonce,
+        name,
+      },
+      {
+        accounts: {
+          multisig: multisig.publicKey,
+          owner: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [multisig]
+      }
+    );
+
+
+    // Step 2: Create mint and two token accounts
+    let decimals = 0
+    let mintAddress = await splToken.createMint(
+      provider.connection,
+      provider.wallet.payer,
+      provider.wallet.publicKey,
+      null,
+      decimals,
+    );
+
+    const mintTestAddress = new anchor.web3.PublicKey(mintAddress);
+
+    const senderTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      program.provider.wallet.payer,
+      mintAddress,
+      program.provider.wallet.publicKey
+    )
+
+    const receiverTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      program.provider.wallet.payer,
+      mintAddress,
+      multisig.publicKey
+    )
+
+    // give 100 tokens to sender token accounts
+    await splToken.mintTo(
+      provider.connection,
+      program.provider.wallet.payer,
+      mintAddress,
+      senderTokenAccount.address,
+      program.provider.wallet.payer,
+      100
+    )
+
+    let mintInfo = await splToken.getMint(provider.connection, mintTestAddress)
+    assert.strictEqual(mintInfo.supply, 100n)
+
+    let senderTokenAccountBalance = await provider.connection.getTokenAccountBalance(senderTokenAccount.address);
+    let receiverTokenAccountBalance = await provider.connection.getTokenAccountBalance(receiverTokenAccount.address);
+    assert.strictEqual(senderTokenAccountBalance.value.amount, '100')
+    assert.strictEqual(receiverTokenAccountBalance.value.amount, '0')
+
+    // send 10 tokens from SenderTokenAccount to ReceiverTokenAccount
+    await splToken.transferChecked(
+      provider.connection,
+      program.provider.wallet.payer,
+      senderTokenAccount.address,
+      senderTokenAccount.mint,
+      receiverTokenAccount.address,
+      program.provider.wallet.publicKey,
+      10,
+      mintInfo.decimals)
+
+    senderTokenAccountBalance = await provider.connection.getTokenAccountBalance(senderTokenAccount.address);
+    receiverTokenAccountBalance = await provider.connection.getTokenAccountBalance(receiverTokenAccount.address);
+    assert.strictEqual(senderTokenAccountBalance.value.amount, '90')
+    assert.strictEqual(receiverTokenAccountBalance.value.amount, '10')
+
+
+    // Step 3: Send money back from ReceiverTokenAccount to SenderTokenAccount by multisig
+    const newTransaction = anchor.web3.Keypair.generate();
+
+    // Change authority. Set owner of ReceiverTokenAccount to multisigSigner
+    await splToken.setAuthority(
+      provider.connection,
+      program.provider.wallet.payer,
+      receiverTokenAccount.address,
+      multisig,
+      AuthorityType.AccountOwner,
+      multisigSigner
+    )
+
+    let transferInstruction = createTransferCheckedInstruction(
+      receiverTokenAccount.address,
+      receiverTokenAccount.mint,
+      senderTokenAccount.address,
+      multisigSigner,
+      10,
+      mintInfo.decimals
+    );
+
+    await program.rpc.createTransaction(
+      {
+        pid: TOKEN_PROGRAM_ID,
+        accs: transferInstruction.keys,
+        data: transferInstruction.data,
+        name: 'transfer_new',
+      },
+      {
+        accounts: {
+          multisig: multisig.publicKey,
+          transaction: newTransaction.publicKey,
+          proposer: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [newTransaction],
+      }
+    );
+
+    let remainingAccounts = transferInstruction.keys
+      .concat({
+        pubkey: TOKEN_PROGRAM_ID,
+        isWritable: false,
+        isSigner: false,
+      })
+
+    // disable all external signers
+    remainingAccounts.forEach(it => it.isSigner = false)
+
+    await program.rpc.executeTransaction({
+      accounts: {
+        multisig: multisig.publicKey,
+        multisigSigner,
+        transaction: newTransaction.publicKey,
+      },
+      remainingAccounts,
+    });
+
+    senderTokenAccountBalance = await provider.connection.getTokenAccountBalance(senderTokenAccount.address);
+    receiverTokenAccountBalance = await provider.connection.getTokenAccountBalance(receiverTokenAccount.address);
+
+    assert.strictEqual(senderTokenAccountBalance.value.amount, '100')
+    assert.strictEqual(receiverTokenAccountBalance.value.amount, '0')
+  });
 });
+
